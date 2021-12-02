a2eaf70cd7541e5538455223d1e135ff67334bc9
muoio <tuana2tqt@gmail.com>
2021-12-02 22:16:26 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 const electron = require('electron');
+const CryptoJS = require('crypto-js');
 
+const axios = require('axios');    
 
-const net = electron.remote.net;
+var hostAPI = 'https://testnet.binancefuture.com'   
+let apiKey = 'd48075f8255c0031acc018dd6be75f8fe6fab02c2fc62502a7d87fac3f160433';
+let apiSecret = '227a661ba00dda99e0fbf5ed2f124ae4e5cc9f5adeddc0fa65c6601f12770d9d';
+var instance = axios.create({
+    baseURL: hostAPI,
+    headers: {
+    "Content-Type": "application/json",
+    "X-MBX-APIKEY": apiKey
+    }
+});
 
+function buy(){
+        const query_string = "symbol=BTCUSDT&side=SELL&type=MARKET&quantity=0.5&timestamp="+Date.now().toString();
+        let signature = CryptoJS.HmacSHA256(query_string, apiSecret).toString();
+        instance.post("/fapi/v1/order?"+query_string+'&signature='+signature)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+}
 
-/*
 const { app, BrowserWindow, ipcMain } = electron;
-
 app.on('ready',()=>{
     console.log("app is now ready");
     const mainWindow = new BrowserWindow({   
@@ -17,51 +37,9 @@ app.on('ready',()=>{
         },
     });
     mainWindow.loadFile("./renderer.html");
-});*/
-function test(){
-    let apiKey = "d48075f8255c0031acc018dd6be75f8fe6fab02c2fc62502a7d87fac3f160433";
-    let apiSecret = "227a661ba00dda99e0fbf5ed2f124ae4e5cc9f5adeddc0fa65c6601f12770d9d";
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+});
 
-    xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        console.log(this.responseText);
-      }
-    });
-
-    xhr.open("POST", "%7B%7Burl%7D%7D/fapi/v1/order?symbol=&side=&type=&quantity=&price=&timestamp=%7B%7Btimestamp%7D%7D&signature=%7B%7Bsignature%7D%7D");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-MBX-APIKEY", apiKey);
-
-    xhr.send();
-}
-/*
-function buy(){
-    let host = "https://testnet.binancefuture.com";
-    let apiKey = "d48075f8255c0031acc018dd6be75f8fe6fab02c2fc62502a7d87fac3f160433";
-    let apiSecret = "227a661ba00dda99e0fbf5ed2f124ae4e5cc9f5adeddc0fa65c6601f12770d9d";
-
-    let timestamp = (new Date()).getTime().toString();
-    let query = "/dapi/v1/order?symbol=&side=&type=MARKET&quantity=0.001&timestamp="+timestamp;
-    let signature = CryptoJS.HmacSHA256(query, apiSecret).toString();
-    var settings = {
-        "url": host+query+"&signature="+signature,
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/json",
-            "X-MBX-APIKEY": apiKey
-        },
-    };
-
-    $.ajax(settings).done(function (response) {
-    console.log(response);
-    });
-}*/
-test();
-/*
 ipcMain.on("action:buy", (event,symbo,amount)=>{
     console.log("buy "+symbo+" with amount "+amount);
     buy();
-});*/
+});
