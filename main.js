2af96f28d88ac340567612a070d136adc9eaffc1
muoio <tuana2tqt@gmail.com>
2021-12-08 12:46:55 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const axios = require('axios');    
 const client = require('./client.js');
 
-const { Binance } = client;
+const { Binance, Strategy } = client;
 var hostWss = "wss://stream.binancefuture.com";
 let apiKey = 'd48075f8255c0031acc018dd6be75f8fe6fab02c2fc62502a7d87fac3f160433';
 let apiSecret = '227a661ba00dda99e0fbf5ed2f124ae4e5cc9f5adeddc0fa65c6601f12770d9d';
@@ -23,16 +23,30 @@ app.on('ready',()=>{
         },
     });
     mainWindow.loadFile("./renderer.html");
+    run();
+});
+
+async function run(){
     var socket = new WebSocket(hostWss+'/ws/btcusdt@miniTicker');
     socket.onmessage = async (event) => {
         var data = JSON.parse(event.data);
         let current_price = parseFloat(data['c']);
         mainWindow.webContents.send("data:price", current_price);
     }
-});
 
-ipcMain.on("action:buy", async (event,symbo,amount) =>  {
-    console.log("buy "+symbo+" with amount "+amount);
-    let bot1 = new Binance(apiKey,apiSecret);
-    await bot1.place_order(symbo,'BUY',"MARKET", amount);
-});
+    let bot1  = new Binance(apiKey,apiSecret);
+    let bot2  = new Binance(apiKey,apiSecret);
+
+    let strategy = new Strategy(bot1,bot2);
+    mainWindow.webContents.send("action:bot1_logined");
+    mainWindow.webContents.send("action:bot2_logined");
+
+
+    ipcMain.on("action:start", async (event, amount) =>  {
+        console.log("buy  with amount "+amount);
+        strategy.start();
+    });
+    ipcMain.on("action:stop", async (event) =>{
+        strategy.stop();
+    });
+}
